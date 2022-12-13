8508f03ec2dd47b2d81c4c48697ba2cd6c9d23ed
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2022-12-12 19:33:53 -0600

    Pass the address and the host as arguments

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Rust_Server
+# Rust Server
 
 A web server with a simple To Do app implemented with Rust.
 
 ## Install
 
-Clone the repository
+Clone the repository.
 
 ```
 git clone https://github.com/suarezsandicamilo/rust_server.git
@@ -12,10 +12,19 @@ git clone https://github.com/suarezsandicamilo/rust_server.git
 
 ## Usage
 
-Run with [Cargo](https://crates.io/crates/deno)
+Run with [Cargo](https://crates.io/crates/deno).
 
 ```
-cargo run
+cargo run -- <address> <port>
+```
+
+`Ctrl + C` to stop the server.
+
+### Example
+
+```
+cargo run -- 127.0.0.1 8080
+Server running at 127.0.0.1:8080
 ```
 
 ## License
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,24 @@
 pub mod app;
 pub mod http;
 
+use std::io::Error;
+use std::io::ErrorKind;
+
 use crate::app::to_to_app::ToDoApp;
 use crate::http::http_server::HttpServer;
 
-fn main() -> Result<(), std::io::Error> {
-    let mut server = HttpServer::new("127.0.0.1", "8080")?;
+fn main() -> Result<(), Error> {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 3 {
+        return Err(Error::new(ErrorKind::InvalidInput, "Not enough arguments"));
+    }
+
+    let address = &args[1];
+
+    let host = &args[2];
+
+    let mut server = HttpServer::new(address, host)?;
 
     let todo_app = Box::new(ToDoApp {});
 
